3d8694568f3bae19589926bcb197f4ee2ed8e1a2
Tom Simnett <tom@initforthe.com>
2014-10-30 17:44:33 +0000

    Remove unneeded line

--- lib/tel_link_rails/view_helper.rb
+++ lib/tel_link_rails/view_helper.rb
@@ -2,7 +2,6 @@ module TelLinkRails
   module ViewHelper
 
     def tel_link(number, name = nil, html_options = {}, &block)
-      sets_of_numbers = []
       sets_of_numbers = number.scan(/[\+0-9]+/)
       num = "#{sets_of_numbers.join('-')}"
 
